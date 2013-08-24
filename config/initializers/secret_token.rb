1cd4f4a641a6a3861c4001423483afab7cf1c5d4
Tayfun Öziş ERİKAN <tayfun.ozis.erikan@lab2023.com>
2013-08-24 10:03:54 +0300

    #1 Rails was initialized.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Elegan::Application.config.secret_key_base = '835ae6bee9cbc6049f000e95a4393b6d26473ca6862050803fe2d39aef9f99b00d37bb782f80cd6226d4245c1caf4dac74d2f22ad87830ca7589faab5278e8fa'
